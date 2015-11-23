134bfcc933d90bd89943facbb0269ec4857e48b3
Tymon <dragomir@localhost.home>
2015-11-23 23:13:25 +0000

    enhanced gallery slideshow on homepage

--- public/javascripts/gallery_alt.js
+++ public/javascripts/gallery_alt.js
@@ -1,28 +1,28 @@
-function createGallery (el, nav, loop) {
-    
+function createGallery (el, nav, loop, onChange) {
+
   'use strict';
-  
+
   var childSelector = "li"; // Child selector
   var preceedingCount = 5; //
-  
+
   var loop = typeof loop === "undefined" ? true : loop;
-  
-  var children = _.map(el.querySelectorAll(childSelector), function (e, i) { 
+
+  var children = _.map(el.querySelectorAll(childSelector), function (e, i) {
     var clone = e.cloneNode(true);
     clone.setAttribute("data-id", i);
     clone.style.position = "absolute";
     clone.style.left = 0;
     clone.style.top = 0;
-    
+
     return clone;
   });
-  
+
   if (children.length === 0) {
     return;
   }
-  
+
   var currentChild = 0;
-  
+
   if (loop) {
     // if we loop we delete all children and then recreate them in appendChildren
     // todo: change this so it doesnt remove all the kids, just append them if necessary
@@ -38,78 +38,103 @@ function createGallery (el, nav, loop) {
       e.style.left = 0;
       e.style.top = 0;
       e.style.marginLeft = 100 * i + "%";
-    }); 
+
+      setTimeout(function() { e.className = "animated-slide"; }, 100);
+    });
   }
-  
+
   var getChildID = function (number) {
     var n = (number + children.length);
     return ((n % children.length) + children.length) % children.length;
   };
-  
+
   var onResize = function () {
     //$el.parent().css("height", $el.find(childSelector).first().height() + "px");
   };
-  
+
   var appendChildren = function () {
     for (var i = currentChild - preceedingCount ; i <= currentChild + preceedingCount ; i++) {
       var toAppend = children[getChildID(i)].cloneNode(true);
       toAppend.style.marginLeft = 100 * i + "%";
       toAppend.setAttribute("data-margin-left", 100 * i);
-      
+
       el.appendChild(toAppend);
-      
+
     }
   };
-  
+
   var addMargin = function (amount) {
     _.each(el.querySelectorAll(childSelector), function (e) {
       var percent = parseInt(e.getAttribute("data-margin-left"), 10) + amount;
-      
+
       e.style.marginLeft = percent + "%";
-      
+
       e.setAttribute("data-margin-left", percent);
     });
   };
-  
+
   var checkNavigation = function () {
     if (loop || _.isNull(nav)) {
       return;
     }
-    
+
     if (currentChild === children.length - 1) {
       nav.querySelector(".next").className = nav.querySelector(".next").className + " inactive";
     }
     else {
       nav.querySelector(".next").className =  nav.querySelector(".next").className.replace(" inactive", "");
     }
-    
+
     if (currentChild === 0) {
       nav.querySelector(".prev").className = nav.querySelector(".prev").className + " inactive";
     }
     else {
       nav.querySelector(".prev").className =  nav.querySelector(".prev").className.replace(" inactive", "");
     }
-    
+
   }
-  
-  var showNext = function () {
-    
-    if (!loop && currentChild === children.length - 1) {
+
+  var goTo = function (id) {
+    if (id < 0 || id > children.length -1) {
       return;
     }
-    
+
+    onChange(id)
+
+    currentChild = getChildID(id);
+
+    _.each(el.querySelectorAll(childSelector), function (e, i) {
+      var percent = (i - id) * 100//parseInt((currentChild - id), 10);
+
+      e.style.marginLeft = percent + "%";
+
+      e.setAttribute("data-margin-left", percent);
+    });
+
+    //addMargin(-100 * change);
+  };
+
+  var showNext = function () {
+
+    if (!loop && currentChild === children.length - 1) {
+      goTo(0);
+      return;
+    }
+
     addMargin(-100);
     currentChild = getChildID(currentChild + 1);
-    
+
+    onChange(currentChild)
+
     if (loop) {
       setTimeout( function () {
         var first = el.querySelector(childSelector + ":first-child");
         first.parentNode.removeChild(first);
       }, 600);
-    
+
       var id = currentChild + preceedingCount;
       var margin = 100 * preceedingCount;
-      
+
       var toAppend = children[getChildID(id)].cloneNode(true);
       toAppend.setAttribute("data-margin-left", margin);
       toAppend.style.marginLeft = margin + "%";
@@ -118,29 +143,29 @@ function createGallery (el, nav, loop) {
     else {
       checkNavigation();
     }
-    
+
     //$el.trigger("change", currentChild);
-    
+
   };
-  
+
   var showPrevious = function () {
-    
+
     if (!loop && currentChild === 0) {
       return;
     }
-    
+
     addMargin(100);
     currentChild = getChildID(currentChild - 1);
-    
+
     if (loop) {
       setTimeout( function () {
         var last = el.querySelector(childSelector + ":last-child");
         last.parentNode.removeChild(first);
       }, 600);
-    
+
       var id = currentChild - preceedingCount;
       var margin = -100 * preceedingCount;
-      
+
       var toPrepend = children[getChildID(id)].cloneNode(true);
       toPrepend.setAttribute("data-margin-left", margin);
       toPrepend.style.marginLeft = margin + "%";
@@ -149,40 +174,41 @@ function createGallery (el, nav, loop) {
     else {
       checkNavigation();
     }
-    
+
     //$el.trigger("change", currentChild);
-    
+
   };
-  
+
   if (loop) {
     appendChildren();
   }
   else {
     checkNavigation();
   }
-  
+
   if (!_.isNull(nav)) {
     nav.querySelector(".next").addEventListener("click", function (event) {
       event.preventDefault();
       showNext();
     });
-    
+
     nav.querySelector(".prev").addEventListener("click", function (event) {
       event.preventDefault();
       showPrevious();
     });
   }
-  
+
   window.addEventListener("resize", onResize);
   window.addEventListener("load", onResize);
-  
-  //$el.trigger("change", currentChild);
-  
-  //$el.showNext = showNext();
-  
-  return {
-    showNext : showNext
-  }
-  
-}
 
+  //$el.trigger("change", currentChild);
+
+  //$el.showNext = showNext();
+
+  return {
+    showNext : showNext,
+    goTo : goTo,
+    slidesCount : children.length
+  }
+
+}
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
   if (document.querySelector(".gallery ul")) {
-    var g = createGallery(document.querySelector(".gallery ul"), document.querySelector(".gallery nav"), true);
+    var galleryNav = document.createElement("nav");
+    var onGalleryChange = function(id) {
+      _.each(galleryNav.querySelectorAll("a"), function(a, i) {
+        a.className = ""
+        if (id == i) {
+          a.className = "active";
+        }
+      })
+    }
+    var g = createGallery(document.querySelector(".gallery ul"), document.querySelector(".gallery nav"), false, onGalleryChange);
 
     var galleryInterval = null;
 
     var createInterval = function () {
       galleryInterval = setInterval(function () { g.showNext(); }, 4000);
     }
+
+    $(".gallery").on("mouseenter", function () { clearInterval(galleryInterval); console.log("enter"); });
+    $(".gallery").on("mouseleave", function () { createInterval(); console.log("leave"); });
+
+
+    galleryNav.className = "gallery-nav";
+    for (var i = 0 ; i < g.slidesCount ; i++) {
+      var navLink = document.createElement("a");
+      navLink.innerHTML = "<span>" + i + "</span>";
+      (function(i) {
+        navLink.addEventListener("click", function(event) { g.goTo(i); });
+      })(i);
+      galleryNav.appendChild(navLink);
+    }
+    onGalleryChange(0);
+
+    var gallery = document.querySelector(".gallery");
+    gallery.parentNode.insertBefore(galleryNav, gallery.nextSibling);
+    $(galleryNav).on("mouseenter", function () { clearInterval(galleryInterval); console.log("enter"); });
+    $(galleryNav).on("mouseleave", function () { createInterval(); console.log("leave"); });
 
     createInterval();
   }
@@ -21,8 +50,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }, 500);
     }
   });
-
-  //$(".gallery nav a").on("mouseenter", function () { clearInterval(galleryInterval); });
-  //$(".gallery nav a").on("mouseleave", function () { createInterval(); });
-
 });
